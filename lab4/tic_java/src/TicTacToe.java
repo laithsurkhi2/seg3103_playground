f8321191c2eebe57395ca3e0d67a32316e057341
laithsurkhi2 <lalsu020@uottawa.ca>
2024-06-09 13:30:37 -0400

    Implement getBoard method to pass the test

--- lab4/tic_java/src/TicTacToe.java
+++ lab4/tic_java/src/TicTacToe.java
@@ -1,5 +1,16 @@
 public class TicTacToe {
+    private String[][] board;
+
+    public TicTacToe() {
+        board = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = "";
+            }
+        }
+    }
+
     public String[][] getBoard() {
-        return new String[0][0];  
+        return board;
     }
 }
