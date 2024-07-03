d820d24ef4024f43136bdeee4d166b13ee4ca0cb
laithsurkhi2 <lalsu020@uottawa.ca>
2024-07-03 13:26:06 -0400

    question 2.4 changes

--- assignment2/grades/lib/grades/calculator.ex
+++ assignment2/grades/lib/grades/calculator.ex
@@ -18,20 +18,7 @@ defmodule Grades.Calculator do
       "EIN"
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)
-
-      cond do
-        mark > 0.895 -> "A+"
-        mark > 0.845 -> "A"
-        mark > 0.795 -> "A-"
-        mark > 0.745 -> "B+"
-        mark > 0.695 -> "B"
-        mark > 0.645 -> "C+"
-        mark > 0.595 -> "C"
-        mark > 0.545 -> "D+"
-        mark > 0.495 -> "D"
-        mark > 0.395 -> "E"
-        true -> "F"
-      end
+      map_grade(mark, :letter)
     end
   end
 
@@ -45,20 +32,7 @@ defmodule Grades.Calculator do
       0
     else
       mark = calculate_grade(avg_labs, avg_homework, midterm, final)
-
-      cond do
-        mark > 0.895 -> 10
-        mark > 0.845 -> 9
-        mark > 0.795 -> 8
-        mark > 0.745 -> 7
-        mark > 0.695 -> 6
-        mark > 0.645 -> 5
-        mark > 0.595 -> 4
-        mark > 0.545 -> 3
-        mark > 0.495 -> 2
-        mark > 0.395 -> 1
-        true -> 0
-      end
+      map_grade(mark, :numeric)
     end
   end
 
@@ -73,13 +47,44 @@ defmodule Grades.Calculator do
 
   # Question 2.2 Helper function to determine if the student failed to participate adequately
   def failed_to_participate?(avg_homework, avg_exams, labs) do
-    num_labs = Enum.count(Enum.reject(labs, &(&1 < 0.25)))
-
-    avg_homework < 0.4 || avg_exams < 0.4 || num_labs < 3
+    Enum.count(Enum.reject(labs, &(&1 < 0.25))) < 3 or avg_homework < 0.4 or avg_exams < 0.4
   end
 
   # Question 2.3 Helper function to calculate the final grade mark
   def calculate_grade(avg_labs, avg_homework, midterm, final) do
     0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
+  end
+
+  # Question 2.4 Helper function to map grade to letter or numeric value
+  def map_grade(mark, :letter) do
+    cond do
+      mark > 0.895 -> "A+"
+      mark > 0.845 -> "A"
+      mark > 0.795 -> "A-"
+      mark > 0.745 -> "B+"
+      mark > 0.695 -> "B"
+      mark > 0.645 -> "C+"
+      mark > 0.595 -> "C"
+      mark > 0.545 -> "D+"
+      mark > 0.495 -> "D"
+      mark > 0.395 -> "E"
+      true -> "F"
+    end
+  end
+
+  def map_grade(mark, :numeric) do
+    cond do
+      mark > 0.895 -> 10
+      mark > 0.845 -> 9
+      mark > 0.795 -> 8
+      mark > 0.745 -> 7
+      mark > 0.695 -> 6
+      mark > 0.645 -> 5
+      mark > 0.595 -> 4
+      mark > 0.545 -> 3
+      mark > 0.495 -> 2
+      mark > 0.395 -> 1
+      true -> 0
+    end
   end
 end
